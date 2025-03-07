45e31007c5cb1b28e7a371e827501e95aa8b130b
Jack Harner <hello@jackharner.com>
2025-03-07 00:50:02 -0700

    tilt

--- src/pages/thanks.tsx
+++ src/pages/thanks.tsx
@@ -2,13 +2,11 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/layout"
 
-import both from "../images/products/blue/tilt.jpg";
-
 const ThanksPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <div className="thanks__wrapper">
-        <img src={both} alt={"2 Blue SUP Holders"} />
+        <img src="/images/products/blue/tilt.jpg" alt={"2 Blue SUP Holders"} />
         <h1>Thanks for your order!</h1>
         <p>Keep an eye on the email you submitted for order updates, and please email us at sup@supholder.com if you have any questions!</p>
       </div>
